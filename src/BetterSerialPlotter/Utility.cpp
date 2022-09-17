b8b74fbd0d4a0c4ddc559f0a0e363266c8e3c296
Nathan <nbd2@rice.edu>
2022-09-17 13:39:52 -0500

    updating linux version to work with USB, ACM, AMA, rfcomm, ircomm, cuau, cuaU

--- src/BetterSerialPlotter/Utility.cpp
+++ src/BetterSerialPlotter/Utility.cpp
@@ -56,21 +56,40 @@ std::vector<std::string> get_serial_ports() //added function to find the present
 #else
 std::vector<int> get_serial_ports() //added function to find the present serial 
 {
+// char *comports[RS232_PORTNR]={"/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyS5",
+//                        "/dev/ttyS6","/dev/ttyS7","/dev/ttyS8","/dev/ttyS9","/dev/ttyS10","/dev/ttyS11",
+//                        "/dev/ttyS12","/dev/ttyS13","/dev/ttyS14","/dev/ttyS15","/dev/ttyUSB0",
+//                        "/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4","/dev/ttyUSB5",
+//                        "/dev/ttyAMA0","/dev/ttyAMA1","/dev/ttyACM0","/dev/ttyACM1",
+//                        "/dev/rfcomm0","/dev/rfcomm1","/dev/ircomm0","/dev/ircomm1",
+//                        "/dev/cuau0","/dev/cuau1","/dev/cuau2","/dev/cuau3",
+//                        "/dev/cuaU0","/dev/cuaU1","/dev/cuaU2","/dev/cuaU3"};
+
+    // these are for searching for each respective type of linux serial connection. The number corresponds
+    // to the difference between the number of the type of comport (i.e. ttyUSB1 is 1) in the comports array above
+    // and the array where it exists.
+
+    static std::pair<std::string,int> usb_string("ttyUSB",16);
+    static std::pair<std::string,int> acm_string("ttyACM",24);
+    static std::pair<std::string,int> ama_string("ttyAMA",22);
+    static std::pair<std::string,int> rfcomm_string("rfcomm",26);
+    static std::pair<std::string,int> ircomm_string("ircomm",28);
+    static std::pair<std::string,int> cuau_string("cuau",30);
+    static std::pair<std::string,int> cuaU_string("cuaU",34);
+
+    static std::vector<std::pair<std::string,int>> serial_pairs = {usb_string, acm_string, ama_string, rfcomm_string, 
+                                                                    ircomm_string, cuau_string, cuaU_string};
+
     std::vector<int> port_nums;
-    std::string usb_string = "ttyUSB";
-    std::string acm_string = "ttyACM";
 
     std::string path = "/sys/class/tty";
-    for (const auto & entry : fs::directory_iterator(path)){
-        if (entry.path().filename().generic_string().find(usb_string) != std::string::npos) {
-            std::string fp = entry.path().filename().generic_string();
-            fp.erase(fp.begin(),fp.begin()+6);
-            port_nums.push_back(std::stoi(fp)+16); // see SerialPort.hpp in mahi-com for enum which makes this + 16
-        }
-        if (entry.path().filename().generic_string().find(acm_string) != std::string::npos) {
-            std::string fp = entry.path().filename().generic_string();
-            fp.erase(fp.begin(),fp.begin()+6);
-            port_nums.push_back(std::stoi(fp)+24); // see SerialPort.hpp in mahi-com for enum which makes this + 24
+    for (const auto & serial_type : serial_pairs){
+        for (const auto & entry : fs::directory_iterator(path)){
+            if (entry.path().filename().generic_string().find(serial_type.first) != std::string::npos) {
+                std::string fp = entry.path().filename().generic_string();
+                fp.erase(fp.begin(),fp.begin()+serial_type.first.length());
+                port_nums.push_back(std::stoi(fp)+serial_type.second); // see SerialPort.hpp in mahi-com for enum which makes this + 16
+            }
         }
     }
         
